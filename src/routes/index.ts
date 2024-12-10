157dffe542adf5aeece114a1c497a3fbd458812d
rafaeld108 <38574440+rafaeld108@users.noreply.github.com>
2024-12-09 23:17:34 -0300

    feat: configured the routes with meta required auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,30 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { authGuard } from '../middlewares/auth'
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         redirect: 'dashboard',
         component: () => import('../layouts/DefaultLayout.vue'),
+        beforeEnter: authGuard,
         children: [
             {
                 path: 'dashboard',
                 name: 'dashboard',
+                meta: { requiresAuth: true },
                 // @ts-ignore
                 component: () => import('../views/Dashboard/DashboardView.vue')
             },
             {
                 path: 'projects',
                 name: 'projects',
+                meta: { requiresAuth: true },
                 component: () => import('../views/Projects/ProjectsView.vue')
             },
             {
                 path: 'categories',
                 name: 'categories',
+                meta: { requiresAuth: true },
                 // @ts-ignore
                 component: () => import('../views/Categories/CategoriesView.vue')
             }
@@ -27,6 +32,7 @@ const routes: Array<RouteRecordRaw> = [
     },
     {
         path: '/login',
+        name: 'login',
         // @ts-ignore
         component: () => import('../views/pages/Login.vue')
     }
