4022299be84d271c03799a34eeab22d3fd071a86
RafaelDias108 <rafaelsd70@gmail.com>
2025-02-08 15:13:41 +0000

    feat: adicionado as configurações globais do plugin de Notificações

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,25 @@ import * as directives from 'vuetify/directives'
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
+// vue-toastification
+import Toast, { PluginOptions, POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
+const options: PluginOptions = {
+    maxToasts: 10,
+    position: POSITION.TOP_RIGHT,
+    newestOnTop: true,
+    timeout: 5000,
+    closeOnClick: true,
+    pauseOnFocusLoss: false,
+    pauseOnHover: false,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: true,
+    hideProgressBar: false,
+    closeButton: "button",
+    icon: true,
+}
+
 import App from './App.vue'
 import router from './routes';
 
@@ -26,4 +45,4 @@ const vuetify = createVuetify({
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate)
 
-createApp(App).use(vuetify).use(router).use(pinia).mount('#app')
+createApp(App).use(vuetify).use(router).use(pinia).use(Toast, options).mount('#app')
