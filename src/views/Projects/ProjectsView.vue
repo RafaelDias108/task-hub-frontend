ca03abe8bd00f914767580d630023ac9976c3f7d
RafaelDias108 <rafaelsd70@gmail.com>
2025-02-08 23:47:07 +0000

    feat: criado a view de projetos

--- src/views/Projects/ProjectsView.vue
+++ src/views/Projects/ProjectsView.vue
@@ -1,11 +1,66 @@
 <template>
-    <div>
-        <h1>Projetos</h1>
-    </div>
+    <v-container :fluid="true">
+        <v-card>
+            <template v-slot:prepend>
+                <div class="mr-14">
+                    <h1 class="font-weight-black">Projetos</h1>
+                    <p class="text-subtitle-1 text-grey-darken-1">9 Projetos</p>
+                </div>
+            </template>
+
+            <template v-slot:append>
+                <v-btn class="bg-indigo">Novo Projeto</v-btn>
+            </template>
+            <v-card-text>
+                <div class="d-flex align-center justify-end">
+                    <span class="pb-3 pr-3">Ordenar por:</span>
+                    <v-select class="text-indigo font-weight-bold" max-width="120" v-model="orderData" variant="underlined" single-line density="compact"
+                        :items="[{ title: 'Nome', value: 'nome' }, { title: 'Data', value: 'data' }]"></v-select>
+                </div>
+                <v-row class="px-10 py-5">
+                    <v-col cols="12" md="4">
+                        <v-card color="#EDF0F4" subtitle="This is a card subtitle"
+                            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
+                            title="This is a title"></v-card>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <v-card color="#EDF0F4" subtitle="This is a card subtitle"
+                            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
+                            title="This is a title"></v-card>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <v-card color="#EDF0F4" subtitle="This is a card subtitle"
+                            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
+                            title="This is a title"></v-card>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <v-card color="#EDF0F4" subtitle="This is a card subtitle"
+                            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
+                            title="This is a title"></v-card>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <v-card color="#EDF0F4" subtitle="This is a card subtitle"
+                            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
+                            title="This is a title"></v-card>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <v-card color="#EDF0F4" subtitle="This is a card subtitle"
+                            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
+                            title="This is a title"></v-card>
+                    </v-col>
+                </v-row>
+                <v-row class="py-5" justify="center">
+                    <v-btn color="#1976D2" variant="text">Ver mais</v-btn>
+                </v-row>
+            </v-card-text>
+        </v-card>
+    </v-container>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const orderData = ref({title: 'Nome', value: 'nome'})
 </script>
 
 <style scoped></style>
