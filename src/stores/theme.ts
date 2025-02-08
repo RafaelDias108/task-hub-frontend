02f7fac4d7b382946ec0ebd30498282a71975fda
RafaelDias108 <rafaelsd70@gmail.com>
2025-02-08 15:10:42 +0000

    feat: alteração de importação

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { Theme } from '../interfaces/themeInterfaces';
+import { Theme } from '../types/themeInterfaces';
 
 export const useThemeStore = defineStore('themeStore', {
 
