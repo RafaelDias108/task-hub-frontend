ef7107f7f6cea1d363875abca8b2bf1ead6351b7
RafaelDias108 <rafaelsd70@gmail.com>
2025-02-10 00:52:51 +0000

    feat: configurado as cores do tema

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 
 // Vuetify
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { vuetify } from './plugins/vuetify';
 
 // Config Pinia
 import { createPinia } from 'pinia';
@@ -33,14 +29,6 @@ const options: PluginOptions = {
 
 import App from './App.vue'
 import router from './routes';
-
-const vuetify = createVuetify({
-    components,
-    directives,
-    icons: {
-        defaultSet: 'mdi', // This is already the default value - only for display purposes
-    },
-})
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate)
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,32 @@
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+export const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi', // This is already the default value - only for display purposes
+    },
+    theme: {
+        themes: {
+            light: {
+                dark: false,
+                colors: {
+                    primary: '#1976D2',
+                    'cardCustom': '#EDF0F4'
+                }
+            },
+            dark: {
+                dark: true,
+                colors: {
+                    primary: '#1976D2',
+                    'cardCustom': '#3E4144'
+                }
+            }
+        }
+    }
+})
