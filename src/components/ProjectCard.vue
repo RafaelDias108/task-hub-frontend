2bbbe3b8c496850ba5ec56dc6f84353a834ab9f9
RafaelDias108 <rafaelsd70@gmail.com>
2025-02-12 01:40:37 +0000

    feat: criado o componente ProjectCard

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,22 +1,94 @@
 <template>
-    <v-card class="bg-cardCustom" title="Projeto 1">
+    <v-card class="bg-cardCustom" :title="props.title">
         <template v-slot:prepend>
-            <v-avatar color="primary">
+            <v-avatar class="mr-3" color="primary">
                 <v-icon icon="mdi-notebook-outline" size="large"></v-icon>
             </v-avatar>
         </template>
 
         <template v-slot:text>
-            content
+            <div class="d-flex justify-space-between mt-3 mb-2">
+                <div class="d-flex ga-3">
+                    <v-icon icon="mdi-chart-timeline"></v-icon>
+                    <span class="text-subtitle-2">Progresso</span>
+                </div>
+                <span class="text-subtitle-2">{{ props.totalTaskDone }}/{{ props.totalTask }}</span>
+            </div>
+            <v-progress-linear color="blue-darken-3" :model-value="props.totalTaskDonePercent" :height="5"></v-progress-linear>
+            <div class="mt-10">
+                <v-chip class="mx-1" label size="small" color="primary" variant="flat" v-for="categorie in props.categories">
+                    {{ categorie.title }}
+                </v-chip>
+            </div>
         </template>
 
         <template v-slot:append>
-            menu
+            <v-menu>
+                <template v-slot:activator="{ props }">
+                    <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+                </template>
+
+                <v-list>
+                    <v-list-item class="v-list-item-custom mx-2" link density="compact" v-for="(item, i) in items"
+                        :key="i" :title="item.title" :prepend-icon="item.props.prependIcon">
+                    </v-list-item>
+                </v-list>
+            </v-menu>
         </template>
     </v-card>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+
+    title: {
+        type: String,
+        required: true
+    },
+    totalTask: {
+        type: Number,
+        required: true
+    },
+    totalTaskDone: {
+        type: Number,
+        required: true,
+        default: () => 0
+    },
+    totalTaskDonePercent: {
+        type: Number,
+        required: true
+    },
+    categories: {
+        type: Array,
+        default: () => {}
+    }
+
+})
+
+const items = ref([
+    {
+        title: 'Editar',
+        value: 1,
+
+        props: {
+            prependIcon: 'mdi-pencil-box-multiple-outline',
+        }
+    },
+    {
+        title: 'Deletar',
+        value: 2,
+        props: {
+            prependIcon: 'mdi-delete',
+        }
+    },
+])
 </script>
 
-<style scoped></style>
+<style scoped>
+.v-list-item-custom:hover {
+    background-color: #1976D2;
+    color: #fff;
+}
+</style>
