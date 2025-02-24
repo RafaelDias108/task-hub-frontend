d01ad85d1164dc8ee2b42b66ab890038ed2b4d2b
RafaelDias108 <rafaelsd70@gmail.com>
2025-02-24 00:16:58 +0000

    feat: Criação do projeto
    - função de comunicação com API para criação de projeto;
    - função para calcular a porcentagem;
    - interfaces Projeto e Categoria;

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,3 +15,7 @@ export const GetAllProjectsApi = async () => {
     const response = await api.get('/projects')
     return response.data;
 }
+export const NewProjectApi = async (data: object) => {
+    const response = await api.post('/projects', data)
+    return response.data;
+}
