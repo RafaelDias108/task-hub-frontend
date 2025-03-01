5cda13542ad836e833f6f8f4ad6dd5352eda381a
RafaelDias108 <rafaelsd70@gmail.com>
2025-03-01 03:53:22 +0000

    refactor: removido o refresh_token para não salvar no lado do cliente

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -6,7 +6,6 @@ export const useAuthStore = defineStore('auth', {
 
     state: (): Auth => ({
         user: null,
-        refresh_token: null,
         access_token: null
     }),
     getters: {
@@ -15,22 +14,28 @@ export const useAuthStore = defineStore('auth', {
     actions: {
         async login(email: string, password: string) {
             const response = await LoginApi(email, password)
+            console.log(response);
             this.user = response.data.user;
-            this.refresh_token = response.data.refresh_token;
+            // this.refresh_token = response.data.refresh_token;
             this.access_token = response.data.access_token;
         },
         async refreshToken() {
             try {
-                const response = await RefreshTokenApi(this.refresh_token);
+                // const response = await RefreshTokenApi(this.refresh_token);
+                const response = await RefreshTokenApi();
                 console.log("refreshToken", response);
+                if(response.status == "success"){
+                    this.access_token = response.data.access_token;
+                }
                 
             } catch (error) {
                 console.log("refreshToken", error);
+                throw new Error("Não foi possível gerar o access_token");
             }
         },
         logout() {
             this.access_token = null;
-            this.refresh_token = null;
+            // this.refresh_token = null;
             this.user = null;
         }
     },
