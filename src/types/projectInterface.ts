6e278522bbd118349f595e8953ce6acc0ccf0256
RafaelDias108 <rafaelsd70@gmail.com>
2025-03-16 05:04:43 +0000

    feat: atualizado o campo uuid da interface Project e criado a interface EditProject

--- src/types/projectInterface.ts
+++ src/types/projectInterface.ts
@@ -1,12 +1,12 @@
 import { Category } from "./Category"
 
 export interface Project {
-    uuid: string
+    uuid_project: string
     name_project: string
     total_tasks: number
     total_tasks_completed: number
     categories: Category[]
-    date_project?: Date
+    date_project?: Date | string
     created_at?: Date
 }
 
@@ -15,4 +15,11 @@ export interface NewProject {
     name_project: string
     categories: Category[]
     date_project: string | null
+}
+
+export interface EditProject {
+    uuid?: string
+    name_project: string
+    categories: Category[]
+    date_project: Date | string | null
 }
