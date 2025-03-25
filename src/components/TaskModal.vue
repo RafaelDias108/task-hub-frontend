17a553e839128233c7d3cf1cc9ff3a2549948984
RafaelDias108 <rafaelsd70@gmail.com>
2025-03-25 02:19:10 +0000

    feat: finalizado o modal de  visualização das tarefas já com a informação quando não existir tarefas criadas

--- src/components/TaskModal.vue
+++ src/components/TaskModal.vue
@@ -1,7 +1,7 @@
 <template>
-    <v-dialog v-model="showDialogTask" width="1000">
+    <v-dialog v-model="showDialogTask" max-width="1300">
         <v-card>
-            <v-toolbar class="py-3 px-5" color="primary" height="120">
+            <v-toolbar class="py-3 px-10" color="primary" height="120">
                 <template #prepend>
                     <v-avatar class="mr-3" color="white" size="large">
                         <v-icon icon="mdi-notebook-outline" size="large" color="primary"></v-icon>
@@ -25,7 +25,7 @@
                 </template>
                 <v-fab location="top right" absolute :offset="true" variant="text" icon="mdi-close" density="compact" style="position: fixed; top: 20px; right: 10px;" @click="showDialogTask = false" />
             </v-toolbar>
-            <v-card-title class="d-flex justify-space-between mt-8 mx-5">
+            <v-card-title class="d-flex justify-space-between mt-8 px-10">
                 <h3>Tarefas</h3>
                 <div class="d-flex align-center justify-end">
                     <span class="pb-3 pr-3 text-subtitle-1">Ordenar por:</span>
@@ -34,6 +34,16 @@
                         :items="[{ title: 'Nome', value: 'nome' }, { title: 'Data', value: 'data' }]"></v-select>
                 </div>
             </v-card-title>
+            <v-card-text>
+                <v-row class="d-flex flex-column justify-center align-center py-10" v-if="!haveTasks && !isLoading">
+                    <v-col class="text-center" cols="12" lg="6">
+                        <v-icon icon="mdi-chart-box-plus-outline mb-2" size="72" color="grey-lighten-1"></v-icon>
+                        <p class="font-weight-bold text-h6">Ainda não há tarefas...</p>
+                        <p class="text-bolder">Por favor, clique abaixo para adicionar uma nova tarefa.</p>
+                        <v-btn class="text-none my-5" color="primary" text="Nova Tarefa" />
+                    </v-col>
+                </v-row>
+            </v-card-text>
         </v-card>
     </v-dialog>
 </template>
@@ -42,7 +52,10 @@
 import { ref } from 'vue';
 
 const showDialogTask = defineModel('showDialogTask', { type: Boolean, required: true })
+
 const orderData = ref({ title: 'Nome', value: 'nome' })
+const haveTasks = ref(false)
+const isLoading = ref(false)
 </script>
 
 <style scoped></style>
