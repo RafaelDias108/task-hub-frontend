2bee3061d8e4a0e4b5bd59832f4b22823686312d
RafaelDias108 <rafaelsd70@gmail.com>
2025-04-08 01:23:23 +0000

    feat: criado o modal que gerencia criação/atualização das tarefas

---
+++ src/components/ModalHandleTask.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-dialog v-model="showModal" width="auto">
+        <v-card min-width="500" title="Adicionar uma nova tarefa">
+            <template v-slot:append>
+                <v-btn icon="mdi-close" variant="text" @click="showModal = false"></v-btn>
+            </template>
+            <template v-slot:text>
+                <v-form>
+                    <v-row class="mx-2 my-2">
+                        <v-col cols="12">
+                            <v-text-field label="Tarefa" required clearable v-model="formTask.title_task"
+                                :disabled="props.isLoading"></v-text-field>
+                            <v-text-field label="Descrição da tarefa" required clearable
+                                v-model="formTask.description_task" :disabled="props.isLoading"></v-text-field>
+                            <v-text-field type="date" label="Data da tarefa" required clearable
+                                v-model="formTask.date_task" :disabled="props.isLoading"></v-text-field>
+                            <v-text-field type="time" label="Hora da tarefa" required clearable
+                                v-model="formTask.time_task" :disabled="props.isLoading"></v-text-field>
+                            <v-btn class="mt-2 text-none" :loading="props.isLoading" color="primary" text="Salvar Tarefa" block
+                                size="large" @click="Submit()" />
+                        </v-col>
+                    </v-row>
+                </v-form>
+            </template>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const emit = defineEmits(['close', 'submit']);
+const showModal = defineModel('showModal', { type: Boolean, required: true })
+const props = defineProps({
+    isLoading: {
+        type: Boolean,
+        default: () => false
+    }
+});
+
+const formTask = reactive({
+    uuid_task: null,
+    fk_id_project: null,
+    title_task: null,
+    description_task: null,
+    date_task: null,
+    time_task: null,
+})
+
+function Submit() {
+
+}
+</script>
+
+<style scoped></style>
