29f47f0a3488659881c94d5d7910aa2fc72ebf02
RafaelDias108 <rafaelsd70@gmail.com>
2025-05-29 02:36:50 +0000

    feat: criado componente CardView para os conteúdos dinâmicos das views

---
+++ src/components/CardView.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-card>
+        <template v-slot:prepend>
+            <div class="mr-14">
+                <h1 class="font-weight-black">{{ props.title }}</h1>
+                <v-skeleton-loader type="list-item" v-if="props.isLoading"></v-skeleton-loader>
+                <p class="text-subtitle-1 text-grey-darken-1" v-else>{{ props.qtd + ' ' + props.title }}</p>
+            </div>
+        </template>
+        <template v-slot:append>
+            <v-btn class="text-none" color="primary" @click="emit('btnAction')">{{ props.btnLabel }}</v-btn>
+        </template>
+        <v-card-text>
+            <slot name="content" />
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+
+const emit = defineEmits(['btnAction'])
+
+const props = defineProps({
+    isLoading: {
+        type: Boolean,
+        default: () => false
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    qtd: {
+        type: Number,
+        default: () => 0
+    },
+    btnLabel: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
+
+<style scoped></style>
--- src/views/Categories/CategoriesView.vue
+++ src/views/Categories/CategoriesView.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
-        <h1>Categorias</h1>
-    </div>
+    <v-container fluid>
+        <CardView title="Categorias" btn-label="Nova Categoria">
+            <template #content>
+                <v-row class="d-flex flex-column justify-center align-center py-10">
+                    <v-col class="text-center" cols="6">
+                        <v-icon icon="mdi-chart-box-plus-outline mb-2" size="72" color="grey-lighten-1"></v-icon>
+                        <p class="font-weight-bold text-h6">Ainda não há categorias...</p>
+                        <p class="text-bolder">Por favor, clique no botão acima para adicionar uma nova categoria.</p>
+                    </v-col>
+                </v-row>
+            </template>
+        </CardView>
+    </v-container>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { Login } from '../../types/loginInterface';
+import CardView from '../../components/CardView.vue';
 
-
-const formLogin = ref<Login>({
-    email: '',
-    password: '',
-})
 </script>
 
 <style scoped></style>
