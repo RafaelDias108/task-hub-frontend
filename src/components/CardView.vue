0a92980755730b2a0a33e8c8bb4073316d6b3867
RafaelDias108 <rafaelsd70@gmail.com>
2025-05-30 02:14:52 +0000

    feat: adicionado subtitulo no CardView

--- src/components/CardView.vue
+++ src/components/CardView.vue
@@ -1,16 +1,16 @@
 <template>
-    <v-card>
+    <v-card max-height="700">
         <template v-slot:prepend>
             <div class="mr-14">
                 <h1 class="font-weight-black">{{ props.title }}</h1>
                 <v-skeleton-loader type="list-item" v-if="props.isLoading"></v-skeleton-loader>
-                <p class="text-subtitle-1 text-grey-darken-1" v-else>{{ props.qtd + ' ' + props.title }}</p>
+                <p class="text-subtitle-1 text-grey-darken-1" v-else>{{ props.subtitle }}</p>
             </div>
         </template>
         <template v-slot:append>
             <v-btn class="text-none" color="primary" @click="emit('btnAction')">{{ props.btnLabel }}</v-btn>
         </template>
-        <v-card-text>
+        <v-card-text class="mt-8">
             <slot name="content" />
         </v-card-text>
     </v-card>
@@ -26,6 +26,10 @@ const props = defineProps({
         default: () => false
     },
     title: {
+        type: String,
+        required: true
+    },
+    subtitle: {
         type: String,
         required: true
     },
