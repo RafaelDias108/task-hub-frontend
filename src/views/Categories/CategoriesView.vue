01c6fe6f0ed92e1d49b29b9d5c1fb61af5275297
RafaelDias108 <rafaelsd70@gmail.com>
2025-06-01 01:14:44 +0000

    feat: adicionado o ModalHandleCategory em CategoriesView e funções de criação e edição da categoria

--- src/views/Categories/CategoriesView.vue
+++ src/views/Categories/CategoriesView.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container fluid>
-        <CardView title="Categorias" :subtitle="ComputedTotalCategory" :is-loading="categoriesStore.isLoading" btn-label="Nova Categoria">
+        <CardView title="Categorias" :subtitle="ComputedTotalCategory" :is-loading="categoriesStore.isLoading" btn-label="Nova Categoria" @btn-action="SetModalNewCategory()">
             <template #content>
                 <v-row class="d-flex flex-column justify-center align-center py-10" v-if="categoriesStore.isLoading">
                     <v-col class="text-center" cols="12" lg="6">
@@ -9,7 +9,7 @@
                 </v-row>
                 <v-row class="d-flex flex-column justify-center align-center py-3 px-10" v-else-if="categoriesStore.categories.length > 0 && !categoriesStore.isLoading">
                     <v-col cols="12">
-                        <ListCategories :categories="categoriesStore.categories"  />
+                        <ListCategories :categories="categoriesStore.categories"  @Edit="SetModalEditCategory"/>
                     </v-col>
                 </v-row>
                 <v-row class="d-flex flex-column justify-center align-center py-10" v-else>
@@ -21,16 +21,24 @@
                 </v-row>
             </template>
         </CardView>
+        <ModalHandleCategory v-model:show-modal="showModalCategory" v-model:category="category"/>
     </v-container>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import CardView from '../../components/CardView.vue';
 import { useCategoriesStore } from '../../stores/categories';
 import ListCategories from '../../components/ListCategories.vue';
+import ModalHandleCategory from '../../components/ModalHandleCategory.vue';
+import { Category } from '../../types/Category';
 
 const categoriesStore = useCategoriesStore()
+
+const showModalCategory = ref(false)
+const category = ref<Category>({
+    name_category: null
+})
 
 const ComputedTotalCategory = computed(() => {
     if(categoriesStore.categories.length == 1){
@@ -39,6 +47,22 @@ const ComputedTotalCategory = computed(() => {
         return `${categoriesStore.categories.length} Categorias`
     }
 })
+
+function SetModalNewCategory() {
+
+    category.value.uuid = null
+    category.value.name_category = null
+    category.value.created_at = null
+
+    showModalCategory.value = true
+}
+
+function SetModalEditCategory(data: Category) {
+    category.value.uuid = data.uuid
+    category.value.name_category = data.name_category
+
+    showModalCategory.value = true
+}
 
 onMounted( async () => {
     await categoriesStore.GetAllCategories()
